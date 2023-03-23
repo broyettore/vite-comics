f84385fc3bba691a6adf2e5f71f1a6b73883e9e0
broyettore <broyettore@gmail.com>
2023-03-23 15:10:10 +0100

    components and scss setup done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
-<script setup>
+<script>
+import header from './components/header.vue';
+import main from './components/main.vue';
+import options from './components/options.vue';
+import footer from './components/footer.vue';
+
+export default {
+    components: {
+        header,
+        main,
+        options,
+        footer
+    }
+}
+
 </script>
 
 <template>
+    <header></header>
+    <main></main>
+    <options></options>
+    <footer></footer>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use "./styles/general.scss" as *;
+
 </style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            // a 
+        }
+    }
+}
+</script>
+
+<template>
+</template>
+
+<style lang="scss" scoped>
+@use "./styles/general.scss" as *;
+
+</style>
